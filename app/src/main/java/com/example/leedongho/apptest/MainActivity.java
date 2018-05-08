2fd32c7c16f66a233399bfe02b6235bab33bec41
pointblank94 <pointblank94@naver.com>
2018-05-08 15:54:29 +0900

    180508 gd

--- app/src/main/java/com/example/leedongho/apptest/MainActivity.java
+++ app/src/main/java/com/example/leedongho/apptest/MainActivity.java
@@ -2,14 +2,22 @@ package com.example.leedongho.apptest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(),"123",Toast.LENGTH_LONG).show();
+            }
+        });
     }
-
-
 }
