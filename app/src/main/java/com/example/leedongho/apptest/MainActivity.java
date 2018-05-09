1286d9122b4c8f22bf3d826ab5ce50f0630110ac
pointblank <pointblank94@naver.com>
2018-05-09 10:17:14 +0900

    180509 test

--- app/src/main/java/com/example/leedongho/apptest/MainActivity.java
+++ app/src/main/java/com/example/leedongho/apptest/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
